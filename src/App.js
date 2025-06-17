1e0620d62f960063de0f9f3e05821ce39c5113df
Lucas Fernandes Paixão dos Santos <paixao.lucas@ifood.com.br>
2025-06-17 15:18:17 -0300

    refactor: Simplifica título e remove CSS do App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import AnimatedForm from './AnimatedForm'; // Importa o novo formulário
-import './App.css'; // Se você tiver um App.css global
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bem-vindo ao Formulário Animado!</h1>
+        <h1>Bem-vindo!</h1>
       </header>
       <main>
         <AnimatedForm />
